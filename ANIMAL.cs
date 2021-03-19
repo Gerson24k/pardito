38d2e09b24db690f2ff1ba483f007854e2f7cc29
GERSON P.M <gersonperez715@gmail.com>
2021-03-18 19:21:50 -0600

    VERCION 22222

---
+++ ANIMAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pardito
+{
+    class ANIMAL
+    {
+        string nombre;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace pardito
 {
     public partial class Form1 : Form
     {
+        List<ANIMAL> animales = new List<ANIMAL>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream stream = new FileStream(@"..\..\nombres.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            //Un ciclo para leer el archivo hasta el final del archivo
+            //Lo leído se va guardando en un control richTextBox
+            while (reader.Peek() > -1)
+            //Esta linea envía el texto leído a un control richTextBox, se puede cambiar para que
+            //lo muestre en otro control por ejemplo un combobox
+            {
+                ANIMAL vehtemp = new ANIMAL();
+                vehtemp.Nombre = reader.ReadLine();
+                animales.Add(vehtemp);
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            reader.Close();
         }
     }
 }
